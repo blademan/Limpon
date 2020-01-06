20dc5cfba4271b03707234f9b4e9094f83709489
Heimik <blademan82@gmail.com>
2020-01-06 13:51:15 +0200

    update

--- js/js.js
+++ js/js.js
@@ -2,3 +2,26 @@ $(function()
 {
 	$('.scroll-pane').jScrollPane();
 });
+
+
+
+$(document).ready(function(){
+	$("#btn-richard").click(function(){
+		$(".btn-richard").show();
+		$("#btn-richard").hide();
+	});    
+});
+
+	$(document).ready(function(){
+		$("#btn-anna").click(function(){
+			$(".btn-anna").show();
+			$("#btn-anna").hide();
+		});    
+	});
+
+		$(document).ready(function(){
+			$("#btn-mark").click(function(){
+				$(".btn-mark").show();
+			$("#btn-mark").hide();
+			});    
+		});
