3dd260f47a60a36dca3db9017480f5747ecc4e3f
Eduards <webcooking.pro@gmail.com>
2020-01-31 21:32:31 +0200

    Update
    Update

--- js/js.js
+++ js/js.js
@@ -25,3 +25,40 @@ $(document).ready(function(){
 			$("#btn-mark").hide();
 			});    
 		});
+
+
+var myModal	= function() {	
+
+	
+
+// Get the modal
+var modal = document.getElementById("myModal");
+var body = document.querySelector("body");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var close = document.querySelector('.close')
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+	body.style.overflow = "hidden";
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+close.onclick = function() {
+	body.style.overflow = "auto";
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+		body.style.overflow = "auto";
+    modal.style.display = "none";
+  }
+}
+
+}
+myModal();
